8009e6cca7fad2789c3dbb2604308b19ae400c3a
Megusta23 <hasan_brkic2003@outlook.com>
2024-05-16 17:37:35 +0200

    final result :>

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
-export { default as Home } from "./Home";
 export { default as TvShowsList } from "./TvShowsList";
 export { default as TvShow } from "./TvShows";
 export { default as MoviesList } from "./MovieList";
